6c5058148fa18a89fda0c135a5b7fb34d563ffb8
chanminswe <chanswe67@gmail.com>
2025-04-24 09:05:10 +0630

    Added Tab Views

---
+++ Appetiziers/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  Appetiziers
+//
+//  Created by Chan Min Swe on 4/24/25.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
