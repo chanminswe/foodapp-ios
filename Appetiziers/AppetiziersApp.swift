f85f03fd0c31245fcd12804f587aafd454f20c81
chanminswe <chanswe67@gmail.com>
2025-04-24 08:57:28 +0630

    Initial Commit

---
+++ Appetiziers/AppetiziersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiziersApp.swift
+//  Appetiziers
+//
+//  Created by Chan Min Swe on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetiziersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetiziersTests/AppetiziersTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiziersTests.swift
+//  AppetiziersTests
+//
+//  Created by Chan Min Swe on 4/24/25.
+//
+
+import Testing
+@testable import Appetiziers
+
+struct AppetiziersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
